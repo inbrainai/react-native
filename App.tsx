bb1b6cbe3b8016471b2a0dbd0f01c6e81813626c
Paul Saulue <psaulue@gmail.com>
2020-04-19 14:16:03 +0100

    update v1.0.1

--- App.tsx
+++ App.tsx
@@ -1,77 +1,81 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
-import inbrain, { InBrainReward } from 'inbrain-surveys';
-import { CLIENT_ID, CLIENT_SECRET, USER_ID, SESSION_UID } from 'react-native-dotenv';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-const BridgeButton = (props: any) => {
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
+import inbrain, { InBrainReward, InitOptions } from 'inbrain-surveys';
+import env from 'react-native-dotenv';
+
+const BridgeButton = (props: {name: String, onPress: () => {}}) => {
   return <TouchableOpacity style={styles.buttonContainer} onPress={props.onPress}>
     <Text style={styles.buttonText}>{props.name}</Text>
   </TouchableOpacity>;
 }
 
-interface IAppState {
-  logs: string[];
-  rewards: InBrainReward[];
-}
+type ComponentState = {rewards: InBrainReward[], logs: String[]};
+type ComponentProps = {};
 
-export default class App extends Component<{},IAppState> {
+type SdkMethod = () => Promise<any>;
 
-  constructor(props:{}) {
+
+export default class App extends Component<ComponentProps, ComponentState> {
+
+  constructor(props: ComponentState) {
     super(props);
     this.state = {rewards: [], logs: []};
   }
 
-  componentDidMount() {
-    const clientId = CLIENT_ID;
-    const clientSecret = CLIENT_SECRET;
-    const sessionUid = SESSION_UID;
-    const userId = USER_ID;
-
-    // Init and setAppUserId
-    this.callBridge('init', () => inbrain.init(clientId, clientSecret, sessionUid) )();   
-    this.callBridge('setAppUserId', () => inbrain.setAppUserId(userId) )(); 
-    
-    // OnClose listener
-    inbrain.setOnCloseListener(() => this.appendLog(`[onClose SUCCESS] => `));
-  }
-
-  /**
-   * Convenient wrapper method to call the SDK React Native methods.
-   * This wrapper will call the SDK method, optionally perform a success callback, and log the output of the bridge or any error.
-   * @param name: user friendly name for the method
-   * @param sdkMethod: inbrain SDKs method to call
-   * @param successCallback: [optional] callback to perform once the promise has been resolved
-   */
-  callBridge = (name: string, sdkMethod: () => Promise<any>, successCallback?: (obj: any) => void) => () => {
-    sdkMethod().then((result:any) => {
+  callBridge = <T extends {}>(name: String, sdkMethod: SdkMethod, successCallback?: (r: T) => void) => () => {
+    return sdkMethod().then((result) => {
       successCallback && successCallback(result);
       this.appendLog(`[${name} SUCCESS] => ${result}`);
     }).catch( (err: any) => {
-      this.appendLog(`[${name} ERROR] => ${err}`);
+      this.appendLog(`[${name} ERROR] => ${err.message || err}`);
     });
   };
 
   // Convenient 'setRetults' callbacks for 'callBridge'
-  void = (obj:any) => {};
+  void = (obj: any) => {};
   setRewards = (rewards: InBrainReward[]) => this.setState({rewards});
-  appendLog = (log: string) => this.setState({ logs: this.state.logs.concat(log)});
+  appendLog = (log: String) => this.setState({ logs: this.state.logs.concat(log)});
+
+  componentDidMount = () => {
+
+    const clientId = env['CLIENT_ID'];
+    const clientSecret = env['CLIENT_SECRET'];
+
+    // Init 
+    const options: InitOptions = {
+      sessionUid: env['SESSION_UID'], 
+      userId: env['USER_ID'], 
+      dataPoints: { gender: 'male', age: '25'},
+      title: "NEW TITLE",
+      navbarColor: "#ff0000",
+      production: false
+    };
+    this.callBridge('init', () => inbrain.init(clientId, clientSecret, options) )();   
+
+    // OnClose listener
+    inbrain.setOnCloseListener(() => this.appendLog(`[onClose SUCCESS] => `));
+    inbrain.setOnCloseListenerFromPage(() => this.appendLog(`[onCloseFromPage SUCCESS] => `));
+  }
 
   render() {
     return (
-    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
 
       <View>
         <Text style={styles.title}>SDK Methods</Text>
         <View style={styles.buttonsContainer}>
           <View style={{flexGrow:1, margin: 2}}>
-            <BridgeButton name="showSurveys" onPress={this.callBridge("showSurveys", () => inbrain.showSurveys() )} />
             <BridgeButton name="getRewards" onPress={this.callBridge("getRewards", () => inbrain.getRewards(), this.setRewards)} />
             <BridgeButton name="confirmRewards" onPress={this.callBridge("confirmRewards", () => inbrain.confirmRewards(this.state.rewards) )} />
-          </View>
-          <View style={{flexGrow:1, margin: 2}}>
-            <BridgeButton name="setTitle" onPress={this.callBridge("setTitle", () => inbrain.setTitle("InBrain Example Webview") )} />
-            <BridgeButton name="setNavbarColor" onPress={this.callBridge("setNavbarColor", () => inbrain.setNavbarColor("ff0000") )} />
-            <BridgeButton name="setButtonColor" onPress={this.callBridge("setButtonColor", () => inbrain.setButtonColor("ffff00") )} />
+            <BridgeButton name="showSurveys" onPress={this.callBridge("showSurveys", () => inbrain.showSurveys() )} />
           </View>
         </View>
       </View>
@@ -129,3 +133,5 @@ const styles = StyleSheet.create({
     color: 'white'
   },
 });
+
+
